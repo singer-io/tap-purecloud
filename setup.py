37a6cf7be71083b264aef07c1d8a75b954f7cb8f
Drew Banin <drew@fishtownanalytics.com>
2018-03-13 17:22:43 -0400

    bump dep

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='tap-purecloud',
       install_requires=[
           'singer-python==0.2.1',
           'backoff==1.3.2',
-          'requests==2.12.4',
+          'requests==2.18.4',
           'python-dateutil==2.6.0',
           'PureCloudPlatformApiSdk==0.45.1.101',
           'PureCloudPlatformClientV2'
