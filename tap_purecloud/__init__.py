44f30cc9adecb93b8a09f9f14828f2a93aa3e0ed
Drew Banin <drew@fishtownanalytics.com>
2018-03-13 17:31:07 -0400

    look back one day for adherence

--- tap_purecloud/__init__.py
+++ tap_purecloud/__init__.py
@@ -385,10 +385,12 @@ def get_user_unit_mapping(users):
 def sync_historical_adherence(config, unit_id, users, first_page):
 
     sync_date = config['start_date']
+    sync_date = sync_date - incr
+
     end_date = datetime.date.today()
     incr = datetime.timedelta(days=1)
 
-    while sync_date < end_date:
+    while sync_date <= end_date:
         logger.info("Syncing historical adherence for {}".format(sync_date))
         next_date = sync_date + incr
 
